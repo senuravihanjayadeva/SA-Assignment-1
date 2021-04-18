1a8d71b5577bb7b6f4f031688f95f655e2c1594e
Dilmi101 <dnimasha971@gmail.com>
2021-04-18 22:49:57 +0530

    Order food added

---
+++ FoodServicePublisher/src/foodservicepublisher/FoodServiceInterfaceImpl.java
@@ -0,0 +1,126 @@
+package foodservicepublisher;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import resturantdb.Database;
+import resturantdb.DatabaseImpl;
+
+public class FoodServiceInterfaceImpl implements FoodServiceInterface {
+
+	private Connection connection = null;
+	private Statement statement = null;
+	private Database database;
+	private ResultSet resultSet;
+	
+	Scanner sc = new Scanner(System.in);
+			
+	public FoodServiceInterfaceImpl() {
+		super();
+		database = new DatabaseImpl();
+		connection = database.getDatabaseConnection();
+	}
+
+	@Override
+	public void insertFood() {
+		Orderfood food = new Orderfood();
+		
+		System.out.println("----------	Imeero Family Restaurant :  Add Food Item into the Menu	----------");
+		
+		System.out.println("Enter Food id : ");
+		food.setId(Integer.parseInt(sc.nextLine().trim()));
+		
+		System.out.println("Enter quantity: ");
+		food.setQuantity(Integer.parseInt(sc.nextLine().trim()));
+		
+		System.out.println("Enter table id : ");
+		food.setTableId(Integer.parseInt(sc.nextLine().trim()));
+		
+		double amnt = food.getQuantity() * getFoodPriceById(food.getId());
+		
+		food.setTotAmnt(amnt);		
+		food.setFoodName(getFoodNameById(food.getId()));
+				
+		String sqlQueryFoodOrder = "INSERT INTO orderfood(tableid, foodname, foodid, quantity, totalprice) "
+				+ "VALUES('"+ food.getTableId() +"', '"+ food.getFoodName() +"', '"+ food.getId() + "', '"+food.getQuantity()+"', '"+food.getTotAmnt()+"')";
+		
+		try {
+			statement = connection.createStatement();
+			statement.executeUpdate(sqlQueryFoodOrder);
+			System.out.println("Food ordered successfully ...");
+		} catch (SQLException exc) {
+			System.out.println("Error with ordering Food Item");
+			System.out.println(exc.getMessage());
+		}
+		
+	}
+	
+	@Override
+	public double getFoodPriceById(int foodId) {
+		double price = 0;
+		String sqlQueryFoodItem = "SELECT * FROM fooditems WHERE id = '"+ foodId +"'";
+		
+		try {
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sqlQueryFoodItem);
+	
+			while(resultSet.next()) {		
+				price = resultSet.getDouble("foodprice");
+			}		
+				
+		} catch (SQLException exc) {
+			System.out.println("Error with get Food Item by Id");
+			System.out.println(exc.getMessage());
+			price = 0.0;
+		}
+		
+		return price;
+	}
+	
+	@Override
+	public String getFoodNameById(int foodId) {
+		
+		String foodName = "";
+		String sqlQueryFoodItem = "SELECT * FROM fooditems WHERE id = '"+ foodId +"'";
+		
+		try {
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sqlQueryFoodItem);
+	
+			while(resultSet.next()) {		
+				foodName = resultSet.getString("foodname");
+			}		
+				
+		} catch (SQLException exc) {
+			System.out.println("Error with get Food Item by Id");
+			System.out.println(exc.getMessage());
+			foodName = "";
+		}
+		
+		return foodName;
+	}
+
+	@Override
+	public void getallfoodorder() {
+		// TODO Auto-generated method stub
+		Orderfood orderfood = new Orderfood();
+		String sqlQueryFoodOrder = "SELECT * FROM orderfood";
+
+		try {
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sqlQueryFoodOrder);
+		      while (resultSet.next()) {  
+		    	  System.out.printf("%20d %20d %20s %20d %20d %20.2f\n",resultSet.getInt("id"),resultSet.getInt("tableid"),resultSet.getString("foodname"),resultSet.getInt("foodid"),
+		    			  resultSet.getInt("quantity"), resultSet.getDouble("totalprice")	  );		    	
+		      }
+		} catch (SQLException exc) {
+			System.out.println("Error with get all Employees");
+			System.out.println(exc.getMessage());
+		}
+	}
+
+}
